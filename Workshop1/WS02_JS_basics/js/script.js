6a53ae991a46491bb86dd48877d25539eb63bde0
SofiaAde <sofia.ade@happysignals.com>
2026-09-13 22:20:01 +0300

    WS02

---
+++ Workshop1/WS02_JS_basics/js/script.js
@@ -0,0 +1,44 @@
+// Exercise 1
+/* console.log("Hello World!")
+alert("Hello World") */
+
+// Exercise 2
+/* const name = "Sofia";
+let age = 26;
+const favAnimal = "Cat";
+
+console.log(name);
+console.log(age);
+console.log(favAnimal);
+
+const sentence = "Hello! My name is" + name + " and my favorit animal is the " + favAnimal + ".";
+console.log(sentence); */
+
+// Exercise 3
+/* const userName = prompt("What is your name?");
+console.log("Hello " + userName); */
+
+//Exercise 4
+/* const ageInput = prompt("How old are you?");
+const userAge = Number(ageInput);
+
+if (userAge >= 18) {
+    console.log("Welcome");
+} else {
+    console.log("You are under 18.");
+} */
+
+// Exercise 5
+/* function greetUser(name) {
+    console.log("Hello " + name);
+}
+
+greetUser("Juho"); */
+
+// Exercise 6
+const myButton = document.getElementById("myButton");
+
+myButton.addEventListener("click", function() {
+    alert("This worked");
+})
+
